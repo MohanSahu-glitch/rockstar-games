5ba00a3147b0f96d4ead9edc0df726c776a369ef
msahu <msahu@contractors.medallia.com>
2025-03-17 17:04:40 +0530

    Moving heading to bottom of the card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,8 +11,7 @@ const GameCard = ({ game }: GameCardProps) => {
         src={getCroppedImageUrl(game.background_image, { x: 600, y: 400 })}
       />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={3}>
           <PlatformIconList
             platforms={
               game.parent_platforms?.map((platform) => platform.platform) || []
@@ -20,6 +19,7 @@ const GameCard = ({ game }: GameCardProps) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
